11d2d7cd9cbe3c8c66b66206c9a4eae334a2c247
Othon Godoy <othon.dev@outlook.com>
2019-05-24 00:35:02 -0300

    TYPESCRIPT

---
+++ aula_00_introducao.ts
@@ -0,0 +1,10 @@
+/**
+ * Aula 00 - Compilando um arquivo TypeScript manualmente
+ * 
+ * Ao criar um arquivo TypeScript, basta abrir no terminal, ou power shell, o diretório em que o arquivo está salvo e executar o sequinte comando:
+ * 
+ * --- tsc nome_do_arquivo.ts
+ */
+
+const a: string = 'Introducao TS';
+console.log(a);
